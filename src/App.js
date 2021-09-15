4ddf2d1f1f0fab0e438dd9c3bd89cf959c742fa9
joibarros <joibarros88@gmail.com>
2021-09-14 21:20:34 -0300

    Incorporación de NavBar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './componentes/NavBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+//componentes clases
 
+//componentes funcionales
 function App() {
+  const jsx = <h1>Johanna Barros</h1>
+  let styles = { 
+    color: 'red'
+  }
+  const stylesJsx = (
+  <div style={styles}>
+    <p>{jsx}</p>
+    </div>
+    )
   return (
-    <div className="App">
+    <div className="App"><NavBar />
       <header className="App-header">
+      
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Johanna Barros
-        </p>
+        {stylesJsx}
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,6 +30,7 @@ function App() {
           Learn React
         </a>
       </header>
+      
     </div>
   );
 }
